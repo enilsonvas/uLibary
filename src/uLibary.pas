26812dfdf854d00ec96d37752c5c9f1226445ea4
enilsonvas <enilson.vas@gmail.com>
2021-05-31 07:36:05 -0300

    AJUSTES

--- src/uLibary.pas
+++ src/uLibary.pas
@@ -2,52 +2,57 @@ unit uLibary;
 
 interface
 
+uses
+  System.NetEncoding;
 type
   TpVerbo = (aGet, aPost, aPut, aDelete, aPacht);
-
   TpRequest = (reqQuery,
-
-               reqCondPagto,
-               reqFormaPagto,
-
+               reqPagtoForma,
+               reqPagtoPrazo,
+               reqCidades,
                reqProd,
                reqProdEmb,
                reqProdPreco,
-
+               reqCli,
+               reqClieEnder,
                reqPedidoId,
                reqPedido,
                reqPedidoItem,
                reqPesqpedidos,
-
                reqPesqCliente,
                reqPesqProduto);
 
-
   function StrTpRequest(aRequest: TpRequest): string;
+  function Encode(aValue: string): string;
+  function Decode(aValue: string): string;
 
 implementation
-
 function StrTpRequest(aRequest: TpRequest): string;
 begin
   case aRequest of
     reqQuery: Result := '/query';
-
-    reqCondPagto  : Result := '/prazopagto';
-    reqFormaPagto : Result := '/formapagto';
-
+    reqPagtoPrazo  : Result := '/prazopagto';
+    reqPagtoForma  : Result := '/formapagto';
+    reqCidades     : Result := '/cidades';
     reqPedidoId   : Result := '/idpedido';
-
     reqPedido     : Result := '/pedido';
     reqPedidoItem : Result := '/pedidoitem';
     reqPesqpedidos: Result := '/pesqpedidos';
-
     reqPesqCliente: Result := '/pesqcliente';
     reqPesqProduto: Result := '/pesqproduto';
-
     reqProd       : Result := '/prod';
     reqProdEmb    : Result := '/prodemb';
     reqProdPreco  : Result := '/prodclasse';
+    reqCli        : Result := '/cli';
+    reqClieEnder  : Result := '/cliender';
   end;
 end;
-
+function Encode(aValue: string): string;
+begin
+  Result := TBase64Encoding.Base64.Encode(aValue);
+end;
+function Decode(aValue: string): string;
+begin
+  Result := TBase64Encoding.Base64.Decode(aValue);
+end;
 end.
